4dbe3212b39c54b01e7346f5466b1a01c2ce3ddb
Susy Maijer <susymayer1996@gmail.com>
2021-06-22 17:34:15 +0200

    S&P500 arch

--- market.py
+++ market.py
@@ -75,22 +75,18 @@ class Market():
         """
         # Get last 20-100 data points
         price_data = pd.DataFrame(self.p[-min(len(self.p)-1, 100):])
-        returns = 100*calc_norm_return(price_data, False)
+        returns = 100*price_data.pct_change().dropna()
 
-        am = arch_model(returns, p=self.garch_param[0], q=self.garch_param[1], rescale=False)
-        res = am.fit(update_freq=1, disp="off")
-        forecasting = res.conditional_volatility
-        # print(forecasting)
-        # forecasts = res.forecast(reindex=True)
+        am = arch_model(returns, p=self.garch_param[0], q=self.garch_param[1])
+        res = am.fit()
+        print("jee")
+        forecasts = res.forecast(reindex=True)
 
-        # print(forecasts.variance)
-        # print(forecasting)
-        #
-        # print(np.sqrt(forecasts.variance.iloc[-1][0]))
-        # sys.exit()
-        # return np.sqrt(forecasts.variance.iloc[-1][0])
-        # print(float(forecasting.values[-1]/(100)**2))
-        return float(forecasting.values[-1]/(100)**2)
+        print(forecasts.variance)
+
+        print(np.sqrt(forecasts.variance.iloc[-1][0]))
+        return np.sqrt(forecasts.variance.iloc[-1][0])
+
 
     def reset_lists(self):
         """
